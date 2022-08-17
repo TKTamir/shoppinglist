5e27983febad2ec2de45f39e1e9564198220e71d
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-08-17 11:10:42 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export default class ShoppingLists extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: [],
+    };
+
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBF7Rt7YJY9IHWY5uanaYUti9LypNJiDmw',
+      authDomain: 'chatup-83ba6.firebaseapp.com',
+      projectId: 'chatup-83ba6',
+      storageBucket: 'chatup-83ba6.appspot.com',
+      messagingSenderId: '95269935264',
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+  componentDidMount() {
+    this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');
+    this.unsubscribe = this.referenceShoppingLists.onSnapshot(this.onCollectionUpdate);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const lists = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      var data = doc.data();
+      lists.push({
+        name: data.name,
+        items: data.items.toString(),
+      });
+    });
+    this.setState({
+      lists,
+    });
+  };
+
+  addList() {
+    this.referenceShoppingLists.add({
+      name: 'TestList',
+      items: ['eggs', 'pasta', 'veggies'],
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>All Shopping Lists</Text>
+        <FlatList
+          data={this.state.lists}
+          renderItem={({ item }) => (
+            <Text style={styles.item}>
+              {item.name}: {item.items}
+            </Text>
+          )}
+        />
+        <Button
+          title="Add List"
+          onPress={() => {
+            this.addList();
+          }}
+        ></Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,
+  },
+  item: {
+    fontSize: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 30,
   },
 });
